fe812af657b2ef23387e7d78b767eaa36bf67c9b
Kateřina Horáková <kate.horakova@gmail.com>
2023-03-06 10:24:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+const navItems = document.querySelector('#nav__items');
+const openNavBtn = document.querySelector('#open_nav-btn');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+openNavBtn.addEventListener('click',() => {
+    navItems.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+});
+
+const closeNav = () => {
+    navItems.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none'
+};
+
+closeNavBtn.addEventListener('click', closeNav);
+
+//close nav menu when a menu item is clicked
+if(window.innerWidth < 1080) {
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {      
+        navItem.addEventListener('click', () => {
+            closeNav();
+        });
+
+    });
+};
+
+//change background color on navBar after scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+
+
+
+
+// Initialize Swiper testimonials section
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    //resposive breakpoints
+    breakpoints:{
+      //when window width is >= 600px
+      600:{
+          slidesPerView:2,
+      },
+      //when window width is >= 1080px
+      1080:{
+          slidesPerView:3,
+      },
+    },
+});
